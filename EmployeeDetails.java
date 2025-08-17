de8d74703ef6ff778cbbbf28a96489b3a7b247e7
arekha88 <arekha88@gmail.com>
2025-08-17 12:56:24 +0530

    Add files via upload
    Day 1 Assignment

---
+++ EmployeeDetails.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+
+    String Name = "John Doe";
+    byte Age = 30;
+    double Salary =55000.75;
+    char Gender='M';
+    boolean AcceptTerms=true;
+    float Rating=4.5f;
+    long MobileNumber=9876543210L;
+    int PinCode=56001;
+    byte LuckyNumber=7;
+    
+    System.out.println("Name is:"+ Name +"\nAge is:"+ Age +"\nSalary is:"+ Salary +"\nGender is:"+ Gender);
+    System.out.println("Accept terms is:"+ AcceptTerms +"\nRating is:"+Rating +"\nMobile number is:"+ MobileNumber +"\n"
+    		+ "Pincode is:"+ PinCode +"\nLuckyNumber is:" +LuckyNumber);
+
+
+		
+	}
+
+}
---
+++ Firefox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float BrowserVersion=100.2f;
+		String BrowserName = "firefox";
+		boolean isVisible = true;
+		int ReleaseYear=1988;
+		char BrowserLogo= 'f';
+		
+		System.out.println("Broswer version is " + BrowserVersion);
+		System.out.println("Browser Name is " + BrowserName);
+		System.out.println("isVisible is " + isVisible);
+		System.out.println("Release Year is " + ReleaseYear);
+		System.out.println("Bropswer Logo is " + BrowserLogo);
+
+
+	}
+
+}
